f53b443c4b77598a6797835f3f632b8f8e27a76a
gmendoza-art <lester12342@gmail.com>
2020-11-02 12:14:02 -0600

    Correccion de llaves primarias

--- Desarrollo/SistemaVentasCatalogo/src/main/java/com/equipoanalisis/SistemaVentasCatalogo/entity/ProductoTienda.java
+++ Desarrollo/SistemaVentasCatalogo/src/main/java/com/equipoanalisis/SistemaVentasCatalogo/entity/ProductoTienda.java
@@ -2,6 +2,8 @@ package com.equipoanalisis.SistemaVentasCatalogo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.persistence.Id;
 
@@ -11,6 +13,7 @@ import javax.persistence.Id;
 public class ProductoTienda {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id_producto_tienda")
 	private Long id_producto_tienda;
 	
